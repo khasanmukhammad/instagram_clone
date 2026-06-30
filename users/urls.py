40e2248c2ea4b4f0b1e5a1d71f5c275e964c29dc
xasan <muhammadovich.09@gmail.com>
2026-06-30 09:37:45 +0500

    instagram clone

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import CreateUserView, VerifyAPIView, GetNewVerification, \
+    ChangeUserInformationView, ChangeUserPhotoView, LoginView, LoginRefreshView, LogoutView, \
+    ForgetPasswordView, ResetPasswordView
+
+urlpatterns = [
+    path('login/', LoginView.as_view()),
+    path('login/refresh/', LoginRefreshView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('signup/', CreateUserView.as_view()),
+    path('verify/', VerifyAPIView.as_view()),
+    path('new-verify/', GetNewVerification.as_view()),
+    path('change-user/', ChangeUserInformationView.as_view()),
+    path('change-user-photo/', ChangeUserPhotoView.as_view()),
+    path('forgot-password/', ForgetPasswordView.as_view()),
+    path('reset-password/', ResetPasswordView.as_view()),
+]
